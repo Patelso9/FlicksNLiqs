57d195aae76078192f5dcec9dddd099913632928
Chelsea Smith <smith2chelsea2@gmail.com>
2021-06-16 11:21:59 -0400

    trying routes made db for login

--- chelsea/login/controllers/html/html-routes.js
+++ chelsea/login/controllers/html/html-routes.js
@@ -7,18 +7,18 @@ router.get("/", (req, res) => {
   console.log("GET /");
   // If the user already has an account send them to the members page
   if (req.user) {
-    res.redirect("/members");
+    res.redirect("/home");
   }
-  res.sendFile(path.join(__dirname, "../../public/signup.html"));
+  res.sendFile(path.join(__dirname, "chelsea/login/public/signup.html"));
 });
 
 router.get("/login", (req, res) => {
   console.log("GET /login");
   // If the user already has an account send them to the members page
   if (req.user) {
-    res.redirect("/members");
+    res.redirect("/home");
   }
-  res.sendFile(path.join(__dirname, "../../public/login.html"));
+  res.sendFile(path.join(__dirname, "chelsea/login/public/login.html"));
 });
 
 // Here we've add our isAuthenticated middleware to this route.
@@ -26,7 +26,7 @@ router.get("/login", (req, res) => {
 router.get("/members", isAuthenticated, (req, res) => {
   console.log("GET /members");
   // console.trace();
-  res.sendFile(path.join(__dirname, "../../public/members.html"));
+  res.sendFile(path.join(__dirname, "Main/html.home.html"));
 });
 
 module.exports = router;
